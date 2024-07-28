4127c3dc05958be9621b669b605412c976f36dbf
Abdullah <bakuviabdullah@gmail.com>
2024-07-28 19:41:13 +0400

    add

--- Coding_Harvard/Final_Project/project.py
+++ Coding_Harvard/Final_Project/project.py
@@ -2,6 +2,9 @@ import random
 import cowsay
 import sys
 
+print("")
+print("Welcome to the game rock/paper/scissors. In this game you will be playing against the computer. If you win 3 times you will enter a quiz which will be made of 10 questions. If you get 8 or more right you will win. If you lose to the computer, well, you can always play again!")
+
 def get_user_choice():
     choices = ['rock', 'paper', 'scissors']
     x = input("Enter your choice (rock/paper/scissors): ").lower()
@@ -43,27 +46,37 @@ def play_game():
         elif "Computer wins!" in result:
             computer_score += 1
         display_scores(user_score, computer_score)
-        if user_score == 5 :
+        if user_score == 3:
             break
         if computer_score == 3:
-            print("you lost,try again")
-            sys.exit()
-    if user_score == 5 and computer_score <= 2:
+            print(cowsay.stegosaurus("you lost,try again"))
+            retry1 = input("Do you want to retry? (yes/no) ").lower()
+            if "yes" in retry1:
+                print("")
+                print("Welcome to the game rock/paper/scissors. In this game you will be playing against the computer. If you win 3 times you will enter a quiz which will be made of 10 questions. If you get 8 or more right you will win. If you lose to the computer, well, you can always play again!")
+                play_game()
+            elif "no" in retry1:
+                print("Well thank you for playing, see you later!")
+                sys.exit()
+            else:
+                sys.exit()
+    if user_score == 3 and computer_score <= 2:
+        print(cowsay.dragon("Great job, you won against the computer, now time for the quiz!"))
         print("Ready for the quiz?")
         planet_quiz()
 
 def planet_quiz():
     questions = {
-        "What is the largest planet in our solar system?": "jupiter",
-        "Which planet is known as the 'Red Planet'?": "mars",
-        "What is the hottest planet in our solar system?": "venus",
-        "Which planet has the most moons?": "jupiter",
-        "What is the smallest planet in our solar system?": "mercury",
-        "Which planet has the most rings?": "saturn",
-        "Which planet is known as the 'Morning Star' or 'Evening Star'?": "venus",
-        "Which planet is closest to the Sun?": "mercury",
-        "What is the farthest planet from the Sun in our solar system?": "neptune",
-        "Which planet has the fastest rotation?": "jupiter"
+        "What is the largest planet in our solar system? ": "jupiter",
+        "Which planet is known as the 'Red Planet'? ": "mars",
+        "What is the hottest planet in our solar system? ": "venus",
+        "Which planet has the most moons? ": "jupiter",
+        "What is the smallest planet in our solar system? ": "mercury",
+        "Which planet has the most rings? ": "saturn",
+        "Which planet is known as the 'Morning Star' or 'Evening Star'? ": "venus",
+        "Which planet is closest to the Sun? ": "mercury",
+        "What is the farthest planet from the Sun in our solar system? ": "neptune",
+        "Which planet has the fastest rotation? ": "jupiter"
     }
     correct_answers = 0
     for question, answer in questions.items():
@@ -72,6 +85,19 @@ def planet_quiz():
             correct_answers += 1
     if correct_answers >= 8:
         print(cowsay.trex("Arggh! Congratulations, you have won the game!!!"))
+        sys.exit()
+    else:
+        print(cowsay.daemon(f"You answered {correct_answers} questions correct but you needed 8 to pass"))
+        retry = input("Do you want to retry? (yes/no) ").lower()
+        if "yes" in retry:
+            print("")
+            print("Welcome to the game rock/paper/scissors. In this game you will be playing against the computer. If you win 3 times you will enter a quiz which will be made of 10 questions. If you get 8 or more right you will win. If you lose to the computer, well, you can always play again!")
+            play_game()
+        elif "no" in retry:
+            print("Well thank you for playing, see you later!")
+            sys.exit()
+        else:
+            sys.exit()
 
 def main():
     play_game()
--- Coding_Harvard/Final_Project/test_project.py
+++ Coding_Harvard/Final_Project/test_project.py
@@ -1,6 +1,14 @@
+import pytest
+from project import determine_winner, display_scores
 
 
-"""def get_user_choice():
-def determine_winner(user_choice, computer_choice):
-def planet_quiz():
-def play_game():"""
+def test_determine_winner():
+    determine_winner("scissors", "paper") == "You win!"
+    determine_winner("rock", "paper") == "Computer wins!"
+    determine_winner("paper", "paper") == "It's a tie!"
+
+def test_display_scores():
+    display_scores(1,0) == "Your score: 1, Computer's score: 0"
+    display_scores(1,2) == "Your score: 1, Computer's score: 2"
+    display_scores(1,3) == "Your score: 1, Computer's score: 3"
+    
